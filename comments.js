40a0e151c0f6e21c6385c1ea8614ce13e3a452d9
Alan Burton <97871740+alanburton9999@users.noreply.github.com>
2024-10-16 05:07:43 +0000

    Copilot third commit 2

--- comments.js
+++ comments.js
@@ -1,35 +1,12 @@
-// create a web server that serves the comments from the comments.json file
-// to the user. The comments should be displayed in a nice way.
+// create a web server
 
-// 1. create a web server
-// 2. read the comments.json file
-// 3. send the comments to the user
+var http = require('http');
+var fs = require('fs');
 
-// 1. create a web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/comments') {
-        // 2. read the comments.json file
-        const filePath = path.join(__dirname, 'comments.json');
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'File not found' }));
-            } else {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Route not found' }));
-    }
+var server = http.createServer(function(req, res) {
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/comments.html', 'utf8');
+  myReadStream.pipe(res);
 });
 
-const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
